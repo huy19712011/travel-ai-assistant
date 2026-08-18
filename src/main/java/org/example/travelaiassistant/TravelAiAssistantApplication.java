71d2a2c4a75cab359100a859dad16166eac4711a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-08-18 10:31:21 +0700

    Spring Boot AI project

---
+++ src/main/java/org/example/travelaiassistant/TravelAiAssistantApplication.java
@@ -0,0 +1,15 @@
+package org.example.travelaiassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAiAssistantApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TravelAiAssistantApplication.class, args);
+        System.out.println("running...");
+    }
+
+}
