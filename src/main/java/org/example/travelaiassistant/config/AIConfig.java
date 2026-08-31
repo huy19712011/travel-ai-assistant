756f4c8fbe0bb120c1095af196fa74ab103c61b4
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-08-31 15:18:30 +0700

    Part 3: Conversational AI - 3. Using JDBC and Postgres to save conversatrions

--- src/main/java/org/example/travelaiassistant/config/AIConfig.java
+++ src/main/java/org/example/travelaiassistant/config/AIConfig.java
@@ -2,6 +2,7 @@ package org.example.travelaiassistant.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.Configuration;
 public class AIConfig {
 
     @Bean
-    public ChatMemory chatMemory() {
+    public ChatMemory chatMemory(ChatMemoryRepository repository) {
 
         return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(repository)
                 .maxMessages(20)
                 .build();
     }
